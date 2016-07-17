c2008f705454de66c971e56f1b4368ebbad1c437
worsun <cainiaoshare163.com>
2016-07-17 14:44:09 +0800

    Rejected commit: Default

---
+++ src/main/java/com/newcoder/service/ToutiaoService.java
@@ -0,0 +1,13 @@
+package com.newcoder.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by WXY on 2016/7/17.
+ */
+@Service
+public class ToutiaoService {
+    public String say(){
+        return "This is from ToutiaoService.";
+    }
+}
